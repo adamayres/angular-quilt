f89ae72cb37e6981cc382fd21929f26ed5456186
adamayres <magicaj@gmail.com>
2014-11-17 18:00:12 -0800

    fix(example): fix relative paths

--- src/quilt.js
+++ src/quilt.js
@@ -23,7 +23,7 @@ angular.module('ngQuilt', ['ngComponent'])
   .directive('ngQuilt', function (ngQuiltConfig, $http, $quiltCache) {
     return {
       restrict: 'EA',
-      templateUrl: 'quilt.tpl.html',
+      templateUrl: '/src/quilt.tpl.html',
       scope: {
         quilt: '=?',
         quiltUrl: '@',
